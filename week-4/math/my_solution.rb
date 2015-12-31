41ed4ff5a90421a9d750a1847b0fb5b52aa80b24
Rin <aaronopsahl@gmail.com>
2015-12-31 01:55:12 -0500

    Add 4.3.2 files

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+	p num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+	p num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+	p num_1 * num_2
+end
+
+def divide(num_1, num_2)
+	p num_1.to_f / num_2.to_f
+end
+
+add(9, 3)
+subtract(9, 3)
+multiply(9, 3)
+divide(9, 3)
