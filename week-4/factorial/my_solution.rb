9aed63a7d4b19d3ce1fe298f533ce293beef8a5a
Rin <aaronopsahl@gmail.com>
2016-01-03 21:32:00 -0800

    Add 4.7.1 files

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,14 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def factorial(number)
+  i = 1
+  while number >= 1
+  	i *= number
+  	number -= 1
+  end
+  return i
+end
